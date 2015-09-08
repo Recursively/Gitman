0f5895faaf34fcc3207b412b3e7443151a4aa39b
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-09 11:24:34 +1200

    implemented transparency layer and test forest

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,7 +3,6 @@ package engineTester;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
-import models.RawModel;
 import models.TexturedModel;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
@@ -24,29 +23,41 @@ public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
 
+        Random random = new Random();
+
         Loader loader = new Loader();
-
-        RawModel model = OBJLoader.loadObjModel("stall", loader);
-        TexturedModel stallModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-        ModelTexture texture = stallModel.getTexture();
-
-        // TODO This is broken
-        texture.setShineDamper(10);
-        texture.setReflectivity(1);
-
-        Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("lana")));
-
+        Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
         Light light = new Light(new Vector3f(3000, 2000, 3000), new Vector3f(1, 1, 1));
         Camera camera = new Camera();
 
-        List<Entity> allStalls = new ArrayList<>();
-        Random random = new Random();
+        TexturedModel stallModel = new TexturedModel(OBJLoader.loadObjModel("tree", loader), new ModelTexture(loader.loadTexture("tree")));
+        ModelTexture treeTexture = stallModel.getTexture();
+        // TODO This is broken
+        treeTexture.setShineDamper(10);
+        treeTexture.setReflectivity(1);
+        List<Entity> allTrees = new ArrayList<>();
+
+        for (int i = 0; i < 300; i++) {
+            float x = random.nextFloat() * 1000;
+            float y = 0;
+            float z = random.nextFloat() * -1000;
+            allTrees.add(new Entity(stallModel, new Vector3f(x, y, z), 0,
+                    0, 0f, 10f));
+        }
+
+        TexturedModel fernModel = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
+                new ModelTexture(loader.loadTexture("fern")));
+        ModelTexture fernTexture = fernModel.getTexture();
+        fernTexture.setShineDamper(10);
+        fernTexture.setReflectivity(1);
+
+        List<Entity> allFerns = new ArrayList<>();
 
         for (int i = 0; i < 1000; i++) {
-            float x = random.nextFloat() * 1000 - 50;
+            float x = random.nextFloat() * 1000;
             float y = 0;
-            float z = random.nextFloat() * -1000 - 50;
-            allStalls.add(new Entity(stallModel, new Vector3f(x, y, z), 0,
+            float z = random.nextFloat() * -1000 ;
+            allFerns.add(new Entity(fernModel, new Vector3f(x, y, z), 0,
                     0, 0f, 1f));
         }
 
@@ -59,8 +70,12 @@ public class MainGameLoop {
             camera.move();
             renderer.processTerrain(terrain);
 
-            for (Entity stall : allStalls) {
-                renderer.processEntity(stall);
+            for (Entity tree : allTrees) {
+                renderer.processEntity(tree);
+            }
+
+            for (Entity fern : allFerns) {
+                renderer.processEntity(fern);
             }
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
