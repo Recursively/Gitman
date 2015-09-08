1bf96fdda5d851b6d50527de796fc240633a40e8
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-08 17:10:38 +1200

    implemented shader lighting

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -26,7 +27,8 @@ public class MainGameLoop {
         ModelTexture texture = new ModelTexture(loader.loadTexture("test_texture"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
+        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
 
         Camera camera = new Camera();
 
@@ -35,6 +37,7 @@ public class MainGameLoop {
             camera.move();
             renderer.prepare();
             shader.start();
+            shader.loadLight(light);
             shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
