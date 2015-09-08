a6e741df87daefa19eaaa121805daf58861a030f
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-08 21:40:19 +1200

    made rendering more efficient by implementing batch entity rendering

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -10,9 +10,8 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
 import textures.ModelTexture;
 
 import java.util.ArrayList;
@@ -25,45 +24,51 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
+//        StaticShader shader = new StaticShader();
+//        Renderer renderer = new Renderer(shader);
 
         RawModel model = OBJLoader.loadObjModel("stall", loader);
-        TexturedModel cubeModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-        ModelTexture texture = cubeModel.getTexture();
+        TexturedModel stallModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+        ModelTexture texture = stallModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(1);
 
         Light light = new Light(new Vector3f(3000, 2000, 3000), new Vector3f(1, 1, 1));
         Camera camera = new Camera();
 
-        List<Entity> allCubes = new ArrayList<>();
+        List<Entity> allStalls = new ArrayList<>();
         Random random = new Random();
 
         for (int i = 0; i < 200; i++) {
             float x = random.nextFloat() * 100 - 50;
             float y = random.nextFloat() * 100 - 50;
             float z = random.nextFloat() * -300;
-            allCubes.add(new Entity(cubeModel, new Vector3f(x, y, z), random.nextFloat() * 180f,
+            allStalls.add(new Entity(stallModel, new Vector3f(x, y, z), random.nextFloat() * 180f,
                     random.nextFloat() * 180f, 0f, 1f));
         }
 
+        //TODO
         Mouse.setGrabbed(true);
 
+
+        MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()) {
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            for (Entity dragon : allCubes) {
-                renderer.render(dragon, shader);
+//            renderer.prepare();
+//            shader.start();
+//            shader.loadLight(light);
+//            shader.loadViewMatrix(camera);
+            for (Entity stall : allStalls) {
+//                renderer.render(dragon, shader);
+                renderer.processEntity(stall);
             }
-            shader.stop();
+            renderer.render(light, camera);
+//            shader.stop();
             DisplayManager.updateDisplay();
         }
 
-        shader.cleanUp();
+        renderer.cleanUp();
+//        shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
