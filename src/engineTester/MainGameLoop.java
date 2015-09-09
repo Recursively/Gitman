28dcdb5af99af3849999701d9c51fefb2512d0cd
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-10 00:32:33 +1200

    implemented basic physics and cleans up resources

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,7 +3,10 @@ package engineTester;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import models.RawModel;
 import models.TexturedModel;
+import objParser.ModelData;
+import objParser.OBJFileLoader;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -24,14 +27,81 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Random random = new Random();
-
         Loader loader = new Loader();
         Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("grass")));
         Light light = new Light(new Vector3f(-3000, 2000, -3000), new Vector3f(1, 1, 1));
         Camera camera = new Camera();
 
-        TexturedModel stallModel = new TexturedModel(OBJLoader.loadObjModel("tree", loader), new ModelTexture(loader.loadTexture("tree")));
-        ModelTexture treeTexture = stallModel.getTexture();
+        ModelData data = OBJFileLoader.loadOBJ("lowPolyTree");
+        RawModel lowPolyTreeModel = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), data.getNormals(),
+                data.getIndices());
+
+        TexturedModel lowPolyTreeTexturedModel = new TexturedModel(lowPolyTreeModel,
+                new ModelTexture(loader.loadTexture("lowPolyTree")));
+        lowPolyTreeTexturedModel.getTexture().setShineDamper(10);
+        lowPolyTreeTexturedModel.getTexture().setReflectivity(1);
+
+        List<Entity> allPolyTrees = new ArrayList<>();
+
+        for (int i = 0; i < 200; i++) {
+            float x = random.nextFloat() * 1000;
+            float y = 0;
+            float z = random.nextFloat() * -1000;
+            if ((x > 400 && x < 600) && (z > -600 && z < -400)) {
+                continue;
+            }
+            allPolyTrees.add(new Entity(lowPolyTreeTexturedModel, new Vector3f(x, y, z), 0,
+                    0, 0f, 1f));
+        }
+
+        ModelData data2 = OBJFileLoader.loadOBJ("grassModel");
+        RawModel grassModel = loader.loadToVAO(data2.getVertices(), data2.getTextureCoords(), data2.getNormals(),
+                data2.getIndices());
+
+        TexturedModel grassModelTexturedModel = new TexturedModel(grassModel,
+                new ModelTexture(loader.loadTexture("grassTexture")));
+        grassModelTexturedModel.getTexture().setShineDamper(10);
+        grassModelTexturedModel.getTexture().setReflectivity(1);
+        grassModelTexturedModel.getTexture().setHasTransparency(true);
+        grassModelTexturedModel.getTexture().setUseFakeLighting(true);
+
+        List<Entity> allGrass = new ArrayList<>();
+
+        for (int i = 0; i < 500; i++) {
+            float x = random.nextFloat() * 1000;
+            float y = 0;
+            float z = random.nextFloat() * -1000;
+            if ((x > 400 && x < 600) && (z > -600 && z < -400)) {
+                continue;
+            }
+            allGrass.add(new Entity(grassModelTexturedModel, new Vector3f(x, y, z), 0,
+                    0, 0f, 1f));
+        }
+
+        ModelData data3 = OBJFileLoader.loadOBJ("stall");
+        RawModel stallModel = loader.loadToVAO(data3.getVertices(), data3.getTextureCoords(), data3.getNormals(),
+                data3.getIndices());
+
+        TexturedModel stallTexturedModel = new TexturedModel(stallModel,
+                new ModelTexture(loader.loadTexture("stallTexture")));
+        stallTexturedModel.getTexture().setShineDamper(10);
+        stallTexturedModel.getTexture().setReflectivity(1);
+
+        List<Entity> allStalls = new ArrayList<>();
+
+        for (int i = 0; i < 25; i++) {
+            float x = random.nextFloat() * 1000;
+            float y = 0;
+            float z = random.nextFloat() * -1000;
+            if ((x > 400 && x < 600) && (z > -600 && z < -400)) {
+                continue;
+            }
+            allStalls.add(new Entity(stallTexturedModel, new Vector3f(x, y, z), 0,
+                    0, 0f, 4f));
+        }
+
+        TexturedModel treeModel = new TexturedModel(OBJLoader.loadObjModel("tree", loader), new ModelTexture(loader.loadTexture("tree")));
+        ModelTexture treeTexture = treeModel.getTexture();
         // TODO This is broken
         treeTexture.setShineDamper(10);
         treeTexture.setReflectivity(1);
@@ -39,17 +109,15 @@ public class MainGameLoop {
 
         List<Entity> allTrees = new ArrayList<>();
 
-        for (int i = 0; i < 41; i++) {
-            for (int j = 0; j < 41; j++) {
-                float x = i * 20;//random.nextFloat() * 1000;
-                float y = 0;
-                float z = -j * 20;//random.nextFloat() * -1000;
-                if ((x > 400 && x < 600) && (z > -600 && z < -400)) {
-                    continue;
-                }
-                allTrees.add(new Entity(stallModel, new Vector3f(x, y, z), 0,
-                        0, 0f, 10f));
+        for (int i = 0; i < 200; i++) {
+            float x = random.nextFloat() * 1000;
+            float y = 0;
+            float z = random.nextFloat() * -1000;
+            if ((x > 400 && x < 600) && (z > -600 && z < -400)) {
+                continue;
             }
+            allTrees.add(new Entity(treeModel, new Vector3f(x, y, z), 0,
+                    0, 0f, 10f));
         }
 
         TexturedModel fernModel = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
@@ -61,7 +129,7 @@ public class MainGameLoop {
 
         List<Entity> allFerns = new ArrayList<>();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 300; i++) {
             float x = random.nextFloat() * 1000;
             float y = 0;
             float z = random.nextFloat() * -1000 ;
@@ -71,18 +139,28 @@ public class MainGameLoop {
 
         ///
 
-        TexturedModel dragonModel = new TexturedModel(OBJLoader.loadObjModel("dragon", loader), new ModelTexture(loader.loadTexture("white")));
+        TexturedModel dragonModel = new TexturedModel(OBJLoader.loadObjModel("dragon", loader),
+                new ModelTexture(loader.loadTexture("white")));
         ModelTexture dragonTexture = dragonModel.getTexture();
-        // TODO This is broken
         dragonTexture.setShineDamper(10);
         dragonTexture.setReflectivity(1);
+
+        TexturedModel bunnyModel = new TexturedModel(OBJLoader.loadObjModel("bunny", loader),
+                new ModelTexture(loader.loadTexture("white")));
+        ModelTexture bunnyTexture = bunnyModel.getTexture();
+        bunnyTexture.setShineDamper(10);
+        bunnyTexture.setReflectivity(1);
+
+        TexturedModel exampleModel = new TexturedModel(OBJLoader.loadObjModel("exampleOBJ", loader),
+                new ModelTexture(loader.loadTexture("white")));
+        ModelTexture exmapleTexture = exampleModel.getTexture();
+        exmapleTexture.setShineDamper(10);
+        exmapleTexture.setReflectivity(1);
 
         ///
 
         //TODO
         Mouse.setGrabbed(true);
-
-        float i = 0;
 
         MasterRenderer renderer = new MasterRenderer();
         while (!Display.isCloseRequested()) {
@@ -90,6 +168,9 @@ public class MainGameLoop {
             renderer.processTerrain(terrain);
 
             renderer.processEntity(new Entity(dragonModel, new Vector3f(500, 0, -500), 0, 0, 0f, 10f));
+            renderer.processEntity(new Entity(bunnyModel, new Vector3f(250, 0, -500), 0, 0, 0f, 10f));
+            renderer.processEntity(new Entity(exampleModel, new Vector3f(0, 0, -500), 0, 0, 0f, 10f));
+
 
             for (Entity tree : allTrees) {
                 renderer.processEntity(tree);
@@ -98,6 +179,19 @@ public class MainGameLoop {
             for (Entity fern : allFerns) {
                 renderer.processEntity(fern);
             }
+
+            for (Entity polyTree : allPolyTrees) {
+                renderer.processEntity(polyTree);
+            }
+
+            for (Entity stall : allStalls) {
+                renderer.processEntity(stall);
+            }
+
+            for (Entity grass : allGrass) {
+                renderer.processEntity(grass);
+            }
+
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -6,13 +6,15 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 
-    private Vector3f position = new Vector3f(0,0,0);
+    private Vector3f position = new Vector3f(0,10,0);
     private float pitch;
     private float roll;
     private float yaw;
 
+    private boolean jumping = false;
 
-    private float speed = 2.5f;
+
+    private float speed = 1f;
 
     public Camera() {
     }
@@ -31,12 +33,12 @@ public class Camera {
         if(Keyboard.isKeyDown(Keyboard.KEY_A)){
             moveFromLook(-1 * speed, 0, 0);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
-            position.y += 1 * speed;
-        }
-        if(Keyboard.isKeyDown(Keyboard.KEY_E)){
-            position.y += -1 * speed;
-        }
+//        if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
+//            position.y += 1 * speed;
+//        }
+//        if(Keyboard.isKeyDown(Keyboard.KEY_E)){
+//            position.y += -1 * speed;
+//        }
         if(Keyboard.isKeyDown(Keyboard.KEY_R)){
             roll += speed;
         }
@@ -49,13 +51,42 @@ public class Camera {
         if(yaw > 360 || yaw < -360){
             yaw = 0;
         }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            if (position.y == 10) {
+                jumping = true;
+            }
+        }
+
+        if (position.y > 30) {
+            jumping = false;
+        }
+        if (jumping) {
+            position.y += 2;
+        }
+
+        gravityPull();
+
+        checkBounds();
+    }
+
+    private void gravityPull() {
+        position.y -= 0.981f;
+    }
+
+    private void checkBounds() {
+        if (position.x < 0) position.x = 0;
+        if (position.x > 1000) position.x = 0;
+        if (position.y < 10) position.y = 10;
+        if (position.z > 0) position.z = 0;
+        if (position.z < -1000) position.z = -1000;
     }
 
     public void moveFromLook(float dx, float dy, float dz) {
         /* Basic Trig, see oscars video for more information :D */
         position.z += dx * (float) Math.cos(Math.toRadians(yaw - 90)) + dz * Math.cos(Math.toRadians(yaw));
         position.x -= dx * (float) Math.sin(Math.toRadians(yaw - 90)) + dz * Math.sin(Math.toRadians(yaw));
-        position.y += dy * (float) Math.sin(Math.toRadians(pitch - 90)) + dz * Math.sin(Math.toRadians(pitch));
+        //position.y += dy * (float) Math.sin(Math.toRadians(pitch - 90)) + dz * Math.sin(Math.toRadians(pitch));
     }
 
     public void moveOld() {
