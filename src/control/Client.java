2cc106b4719bd7172b2501d4b7731e840b00b141
Reuben Puketapu <reuben.puketapu@gmail.com>
2015-09-14 12:33:16 +1200

    added simple messages sending between client and server

--- src/control/Client.java
+++ src/control/Client.java
@@ -1,5 +1,59 @@
 package control;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client extends Thread {
+
+	private final Socket socket;
+	private DataOutputStream output;
+	private DataInputStream input;
+
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run() {
+		try {
+
+			output = new DataOutputStream(socket.getOutputStream());
+			input = new DataInputStream(socket.getInputStream());
+
+			Scanner scanner = new Scanner(System.in);
+
+			while (1 == 1) {
+				String text = scanner.next();
+				output.writeUTF(text);
+
+				if (text.equals("EXIT")) {
+					break;
+				}
+
+			}
+
+			scanner.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		int port = 32768; // default
+		Socket sock = null;
+		try {
+			// host name and
+			sock = new Socket("localhost", port);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		new Client(sock).start();
+		;
+	}
 
 }
--- src/control/Server.java
+++ src/control/Server.java
@@ -1,5 +1,54 @@
 package control;
 
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread {
+
+	private Socket socket;
+
+	private DataInputStream inputStream;
+	private DataOutputStream outputStream;
+
+	public Server(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run() {
+		try {
+
+			inputStream = new DataInputStream(socket.getInputStream());
+			outputStream = new DataOutputStream(socket.getOutputStream());
+
+			while (1 == 1) {
+				String read = inputStream.readUTF();
+				System.out.println(socket.getLocalSocketAddress() + ": " + read);
+				
+				outputStream.writeUTF("HELLO");
+
+				if (read.equals("EXIT")) {
+					break;
+				}
+
+			}
+			// server.close();
+			// socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket ss = new ServerSocket(32768);
+		while (true) {
+			Socket socket = ss.accept();
+			new Server(socket).start();
+
+		}
+	}
 
 }
