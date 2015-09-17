d268dc089531079e4cca2ed494e287dc9eedc346
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-18 11:14:04 +1200

    updated frame title

--- src/view/DisplayManager.java
+++ src/view/DisplayManager.java
@@ -14,37 +14,13 @@ public class DisplayManager {
     private static float delta;
 
     public static void createDisplay() {
-        ContextAttribs attribs = new ContextAttribs(3,2)
+        ContextAttribs contextAttribs = new ContextAttribs(3,2)
                 .withForwardCompatible(true).withProfileCore(true);
 
-
-//         Full screen code
-
-//        DisplayMode[] modes = new DisplayMode[0];
-//        try {
-//            modes = Display.getAvailableDisplayModes();
-//        } catch (LWJGLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        DisplayMode current = modes[0];
-//
-//        for (int i=0;i<modes.length;i++) {
-//            current = modes[i];
-//            System.out.println(current.getWidth() + "x" + current.getHeight() + "x" +
-//                    current.getBitsPerPixel() + " " + current.getFrequency() + "Hz");
-//        }
-//
-//        WIDTH = current.getWidth();
-//        HEIGHT = current.getHeight();
-
         try {
-//            Display.setDisplayMode(current);
-//            Display.setFullscreen(true);
-//            Display.create(new PixelFormat(), attribs);
             Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-            Display.create(new PixelFormat(), attribs);
-            Display.setTitle("Our first display");
+            Display.create(new PixelFormat(), contextAttribs);
+            Display.setTitle("Gitman: An EXCEPTIONal Adventure");
 
             Display.setVSyncEnabled(true);
         } catch (LWJGLException e) {
@@ -73,5 +49,65 @@ public class DisplayManager {
 
     private static long getCurrentTime() {
         return Sys.getTime() * 1000 / Sys.getTimerResolution();
+    }
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @param fullscreen
+     */
+    public static void setDisplayMode(int width, int height, boolean fullscreen) {
+
+        // return if requested DisplayMode is already set
+        if ((Display.getDisplayMode().getWidth() == width) &&
+                (Display.getDisplayMode().getHeight() == height) &&
+                (Display.isFullscreen() == fullscreen)) {
+            return;
+        }
+
+        try {
+            DisplayMode targetDisplayMode = null;
+
+            if (fullscreen) {
+                DisplayMode[] modes = Display.getAvailableDisplayModes();
+                int freq = 0;
+
+                for (int i=0;i<modes.length;i++) {
+                    DisplayMode current = modes[i];
+
+                    if ((current.getWidth() == width) && (current.getHeight() == height)) {
+                        if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+                            if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+                                targetDisplayMode = current;
+                                freq = targetDisplayMode.getFrequency();
+                            }
+                        }
+
+                        // if we've found a match for bpp and frequence against the
+                        // original display mode then it's probably best to go for this one
+                        // since it's most likely compatible with the monitor
+                        if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+                                (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+                            targetDisplayMode = current;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                targetDisplayMode = new DisplayMode(width,height);
+            }
+
+            if (targetDisplayMode == null) {
+                System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+                return;
+            }
+
+            Display.setDisplayMode(targetDisplayMode);
+            Display.setFullscreen(fullscreen);
+
+        } catch (LWJGLException e) {
+            System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+        }
     }
 }
