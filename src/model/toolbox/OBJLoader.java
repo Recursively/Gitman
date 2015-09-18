516f6bd10fa8431a1ca7c24456c0ac17de0100f7
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-18 18:14:46 +1200

    refactored objloader

--- src/model/toolbox/OBJLoader.java
+++ src/model/toolbox/OBJLoader.java
@@ -11,9 +11,79 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OBJLoader {
+/**
+ * Loader class to handle the loading of .obj object models into the engine
+ *
+ * @author Marcel van Workum
+ */
+public final class OBJLoader {
 
+    private static List<Vector3f> vertices;
+    private static List<Vector2f> textures;
+    private static List<Integer> indices;
+    private static List<Vector3f> normals;
+    private static float[] verticesArray;
+    private static float[] normalsArray;
+    private static float[] texturesArray;
+    private static int[] indicesArray;
+
+    /**
+     * Creates a new RawModel from an wavefront (.obj) file
+     *
+     * @param fileName FileName
+     * @param loader Loader to load the object into a RawModel
+     *
+     * @return RawModel created
+     */
     public static RawModel loadObjModel(String fileName, Loader loader) {
+
+        // initialises the obj data structures
+        initDataStructures();
+
+        // reads the object file
+        BufferedReader reader = null;
+        try {
+            reader = getObjectFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.err.println("Failed to load object model :"+fileName);
+        }
+
+        // parses the obj object
+        parseObject(reader);
+
+        // initialises vertices and indices arrays after file is read to know sizes
+        verticesArray = new float[vertices.size() * 3];
+        indicesArray = new int[indices.size()];
+
+        // finally load the vertices and indices
+        loadVertices();
+        loadIndices();
+
+        // returns the obj raw model loaded by the loader
+        return loader.loadToVAO(verticesArray, texturesArray, normalsArray, indicesArray);
+    }
+
+    /**
+     * Simply initialises the data structures
+     */
+    private static void initDataStructures() {
+        vertices = new ArrayList<>();
+        textures = new ArrayList<>();
+        normals = new ArrayList<>();
+        indices = new ArrayList<>();
+        normalsArray = null;
+        texturesArray = null;
+    }
+
+    /**
+     * Attempts to read the wavefront file
+     *
+     * @param fileName obj file name
+     *
+     * @return Buffered reader containing the file
+     * @throws FileNotFoundException If the file failed to read
+     */
+    private static BufferedReader getObjectFile(String fileName) throws FileNotFoundException {
         FileReader fr = null;
         try {
             fr = new FileReader(new File("res/"+fileName+".obj"));
@@ -21,33 +91,50 @@ public class OBJLoader {
             System.err.println("Couldn't load file!");
             e.printStackTrace();
         }
-        BufferedReader reader = new BufferedReader(fr);
+
+        // fail shouldn't be null
+        if (fr != null) {
+            return new BufferedReader(fr);
+        }
+
+        // however if it is, shit has gone wrong
+        throw new FileNotFoundException("Failed to load object model");
+    }
+
+    /**
+     * Parses the .obj wavefront into the internal data structures
+     *
+     * @param reader buffered reader with .obj file
+     */
+    private static void parseObject(BufferedReader reader) {
         String line;
-        List<Vector3f> vertices = new ArrayList<>();
-        List<Vector2f> textures = new ArrayList<>();
-        List<Vector3f> normals = new ArrayList<>();
-        List<Integer> indices = new ArrayList<>();
-        float[] verticesArray = null;
-        float[] normalsArray = null;
-        float[] texturesArray = null;
-        int[] indicesArray = null;
         try {
             while (true) {
                 line = reader.readLine();
                 String[] currentLine = line.split(" ");
+
+                // vertex location
                 if (line.startsWith("v ")) {
                     Vector3f vertex = new Vector3f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]),
                             Float.parseFloat(currentLine[3]));
                     vertices.add(vertex);
-                } else if (line.startsWith("vt ")) {
+                }
+
+                // vertex texture location
+                else if (line.startsWith("vt ")) {
                     Vector2f texture = new Vector2f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]));
                     textures.add(texture);
-                } else if (line.startsWith("vn ")) {
+                }
+
+                // vertex normal
+                else if (line.startsWith("vn ")) {
                     Vector3f normal = new Vector3f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]),
                             Float.parseFloat(currentLine[3]));
                     normals.add(normal);
-                } else if (line.startsWith("f ")) {
-                    //TODO doesn't parse models that contain multiple materials
+                }
+
+                // end of vertex information
+                else if (line.startsWith("f ")) {
                     texturesArray = new float[vertices.size() * 2];
                     normalsArray = new float[vertices.size() * 3];
                     break;
@@ -55,43 +142,64 @@ public class OBJLoader {
             }
 
             while (line != null) {
+                // if there is an empty line
                 if (!line.startsWith("f ")) {
                     line = reader.readLine();
                     continue;
                 }
+
+                // read the face values and process
+                // face == f 3/1/2 etc
                 String[] currentLine = line.split(" ");
                 String[] vertex1 = currentLine[1].split("/");
                 String[] vertex2 = currentLine[2].split("/");
                 String[] vertex3 = currentLine[3].split("/");
 
-                processVertex(vertex1, indices, textures, normals, texturesArray, normalsArray);
-                processVertex(vertex2, indices, textures, normals, texturesArray, normalsArray);
-                processVertex(vertex3, indices, textures, normals, texturesArray, normalsArray);
+                // process the three faces on each line
+                processVertex(vertex1, textures, normals);
+                processVertex(vertex2, textures, normals);
+                processVertex(vertex3, textures, normals);
+
+                // read next line
                 line = reader.readLine();
             }
             reader.close();
         }catch (Exception e) {
             e.printStackTrace();
+            System.err.println("Filed to load object model in the OBJLoader");
         }
+    }
 
-        verticesArray = new float[vertices.size() * 3];
-        indicesArray = new int[indices.size()];
+    /**
+     * Loads the indices
+     */
+    private static void loadIndices() {
+        for (int i = 0; i < indices.size(); i++) {
+            indicesArray[i] = indices.get(i);
+        }
+    }
 
+    /**
+     * Loads the xyz vertex points into the vector array
+     */
+    private static void loadVertices() {
         int vertexPointer = 0;
         for(Vector3f vertex : vertices) {
             verticesArray[vertexPointer++] = vertex.x;
             verticesArray[vertexPointer++] = vertex.y;
             verticesArray[vertexPointer++] = vertex.z;
         }
-
-        for (int i = 0; i < indices.size(); i++) {
-            indicesArray[i] = indices.get(i);
-        }
-        return loader.loadToVAO(verticesArray, texturesArray, normalsArray, indicesArray);
     }
 
-    private static void processVertex(String[] vertexData, List<Integer> indices, List<Vector2f> textures,
-                                      List<Vector3f> normals, float[] texturesArray, float[] normalsArray) {
+    /**
+     * Processes each of the obj faces into a corresponding vertex
+     *
+     * @param vertexData Vertex points
+     * @param textures vertex textures
+     * @param normals vertex normals
+     */
+    private static void processVertex(String[] vertexData, List<Vector2f> textures,
+                                      List<Vector3f> normals) {
         int currentVertexPointer = Integer.parseInt(vertexData[0]) - 1;
         indices.add(currentVertexPointer);
         Vector2f currentTexture = textures.get(Integer.parseInt(vertexData[1]) -1);
