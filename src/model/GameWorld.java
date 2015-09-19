19a93bcad9b802504f8c1336b7a5c7393b489bbe
Divya <1divyap1@gmail.com>
2015-09-19 14:22:27 +1200

    Adding game state fields and methods to Game World

--- src/model/GameWorld.java
+++ src/model/GameWorld.java
@@ -3,8 +3,10 @@ package model;
 import model.entities.Camera;
 import model.entities.Entity;
 import model.entities.Light;
+import model.entities.movableEntity.Item;
 import model.entities.movableEntity.Player;
 import model.factories.*;
+import model.guiComponents.Inventory;
 import model.terrains.Terrain;
 import model.textures.GuiTexture;
 import model.toolbox.Loader;
@@ -14,9 +16,11 @@ import java.util.ArrayList;
 /**
  * Delegate class used to represent all the current components of the game world.
  *
- * @author Marcel van Workum
+ * @author Marcel van Workum, Divya
  */
 public class GameWorld {
+	private static final int START_PATCH = 10;   // starting progress value for patch
+	private static final int CODE_VALUE = 10;    // value to increment code progress by
 
     // collection of entities in the game
     private ArrayList<Entity> staticEntities;
@@ -44,11 +48,18 @@ public class GameWorld {
     private Loader loader;
     private Terrain terrain;
 
-
     private ArrayList<GuiTexture> guiImages;
 
-
     private Player player;
+    
+    // game state elements
+    private Inventory inventory;
+    private int codeProgress;        // code collection progress
+    private int patchProgress;       // commit collection progress
+    private Item pickedUp;
+    
+    // overall score
+    private int score;
 
     public GameWorld(Loader loader) {
         this.loader = loader;
@@ -69,6 +80,10 @@ public class GameWorld {
 
         PlayerFactory playerFactory = new PlayerFactory(this);
         player = playerFactory.getPlayer();
+        
+        // game state
+        inventory = new Inventory();
+        this.patchProgress = START_PATCH;
     }
 
     public ArrayList<Light> getLights() {
@@ -82,4 +97,46 @@ public class GameWorld {
     public ArrayList<GuiTexture> getGuiImages() {
         return guiImages;
     }
+    
+    public void decreasePatch(int percent){
+    	double value = percent/100;
+    	this.patchProgress = (int) (this.patchProgress*(1-value));
+    }
+    
+    public void incrementPatch(int percent){
+    	double value = percent/100;
+    	this.patchProgress = (int) (this.patchProgress*(1+value));
+    }
+    
+    public void updateCodeProgress(){
+    	this.codeProgress = this.codeProgress + CODE_VALUE;
+    	
+    	// player has cloned all bits of code
+    	if(CODE_VALUE >= 100){
+    		compileProgram();
+    	}
+    }
+    
+    /**
+     * Updates game score (players get points
+     * for interacting with items)
+     * @param n
+     */
+    public void updateScore(int n){
+    	this.score = this.score + n;
+    }
+    
+    public void pickUpItem(Item i){
+    	this.pickedUp = i;
+    }
+    
+    public void dropItem(){
+    	this.pickedUp = null;
+    }
+
+    // TODO method called when player should be given
+    // options to compile and run program
+	private void compileProgram() {
+		
+	}
 }
--- src/model/guiComponents/Inventory.java
+++ src/model/guiComponents/Inventory.java
@@ -4,11 +4,28 @@ import java.util.ArrayList;
 
 import model.entities.movableEntity.Item;
 
+/**
+ * 
+ * @author Divya
+ *
+ */
 public class Inventory {
 	private ArrayList<Item> inLaptop;
 	
 	public Inventory (){
 		inLaptop = new ArrayList<Item>();
+	}
+	
+	public void addItem(Item i){
+		inLaptop.add(i);
+	}
+	
+	public void deleteItem(Item i){
+		inLaptop.remove(i);
+	}
+	
+	public ArrayList<Item> getInventory(){
+		return inLaptop;
 	}
 
 }
