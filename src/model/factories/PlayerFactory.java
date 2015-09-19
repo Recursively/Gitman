9d0be01803d1d9a3c22cd89dbbb6827637c711f1
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-19 13:00:56 +1200

    fixed player factory

--- src/model/factories/PlayerFactory.java
+++ src/model/factories/PlayerFactory.java
@@ -5,17 +5,39 @@ import model.entities.Camera;
 import model.entities.movableEntity.Player;
 import org.lwjgl.util.vector.Vector3f;
 
+/**
+ * Factory class to handle the creation of Players in the game.
+ *
+ * Handles both the main player and other multilayer players
+ *
+ * @author Marcel van Workum
+ */
 public class PlayerFactory {
 
+    // Reference to parent
     private final GameWorld gameWorld;
 
+    /**
+     * Construct the factory and set the game world
+     *
+     * @param gameWorld Game world
+     */
     public PlayerFactory(GameWorld gameWorld) {
         this.gameWorld = gameWorld;
 
     }
 
+    /**
+     * Creates a new Main player at a given position
+     *
+     * @param position position to create the player at
+     *
+     * @return Player with Camera
+     */
     public Player makeNewMainPlayer(Vector3f position) {
+        // where on the ground should the player be
         float initialPlayerY = gameWorld.getTerrain().getTerrainHeight(position.getX(), position.getZ());
+        position.y = initialPlayerY;
 
         // New player and camera to follow the player
         return new Player(null, position, 0, 180f, 0, 1, new Camera(initialPlayerY, position));
