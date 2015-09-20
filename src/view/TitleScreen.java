0c4492412bbc375977fdbadb17cd4fb867b233ff
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-20 19:27:10 +1200

    fixed display error

--- src/view/TitleScreen.java
+++ src/view/TitleScreen.java
@@ -1,77 +1,75 @@
 package view;
 
+import controller.GameController;
+import model.textures.GuiTexture;
+import model.toolbox.Loader;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector2f;
+import view.renderEngine.GuiRenderer;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.util.vector.Vector2f;
-
-import controller.GameController;
-import model.textures.GuiTexture;
-import model.toolbox.Loader;
-import view.renderEngine.GuiRenderer;
-
 /**
  * Produces the title screen for the game
- * 
- * @author Ellie
  *
+ * @author Ellie
  */
 
 public class TitleScreen {
 
-	private final GuiRenderer guiRenderer;
-	private final Loader loader;
+    private final GuiRenderer guiRenderer;
+    private final Loader loader;
 
-	public TitleScreen() {
+    public TitleScreen() {
 
-		DisplayManager.createDisplay();
-		loader = new Loader();
-		guiRenderer = new GuiRenderer(loader);
-		blinkTitle();
+        DisplayManager.createDisplay();
+        loader = new Loader();
+        guiRenderer = new GuiRenderer(loader);
+        blinkTitle();
 
-	}
+    }
 
-	private void blinkTitle() {
+    private void blinkTitle() {
 
-		long timer = System.currentTimeMillis();
-		int index = 0;
-		GuiTexture[] images = initTitleScreens();
+        long timer = System.currentTimeMillis();
+        int index = 0;
+        GuiTexture[] images = initTitleScreens();
 
-		while (!Display.isCloseRequested()) {
+        while (true) {
 
-			long currentTime = System.currentTimeMillis();
-			if (currentTime - timer > 500) {
-				 index++;
-				 timer += 500;
-			}
-			List<GuiTexture> guiList = new ArrayList<GuiTexture>();
-			guiList.add(images[index % 2]);
-			guiRenderer.render(guiList);
-			DisplayManager.updateDisplay();
-			
-		 if (Keyboard.isKeyDown(Keyboard.KEY_RETURN)) {
-		            DisplayManager.closeDisplay();
-		            new GameController();
-		        }
-			
-		}
-	}
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - timer > 500) {
+                index++;
+                timer += 500;
+            }
+            List<GuiTexture> guiList = new ArrayList<GuiTexture>();
+            guiList.add(images[index % 2]);
+            guiRenderer.render(guiList);
+            DisplayManager.updateDisplay();
 
-	/**
-	 * @return an Array of images to render
-	 */
-	private GuiTexture[] initTitleScreens() {
-		GuiTexture[] images = new GuiTexture[2];
-		images[0] = new GuiTexture(loader.loadTexture("screenimages" + File.separator + "GitmanTitle1"),
-				new Vector2f(0, 0), new Vector2f(1, 1));
+            if (Keyboard.isKeyDown(Keyboard.KEY_RETURN)) {
+                DisplayManager.closeDisplay();
+                break;
+            }
+        }
 
-		images[1] = new GuiTexture(loader.loadTexture("screenimages" + File.separator + "GitmanTitle2"),
-				new Vector2f(0, 0), new Vector2f(1, 1));
-		return images;
-	}
+        new GameController();
+    }
+
+    /**
+     * @return an Array of images to render
+     */
+    private GuiTexture[] initTitleScreens() {
+        GuiTexture[] images = new GuiTexture[2];
+        images[0] = new GuiTexture(loader.loadTexture("screenimages" + File.separator + "GitmanTitle1"),
+                new Vector2f(0, 0), new Vector2f(1, 1));
+
+        images[1] = new GuiTexture(loader.loadTexture("screenimages" + File.separator + "GitmanTitle2"),
+                new Vector2f(0, 0), new Vector2f(1, 1));
+        return images;
+    }
 
 
 }
