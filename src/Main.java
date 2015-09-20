fafbe882fe886e5526d44f74e97fcfb79af5e274
Reuben Puketapu <reuben.puketapu@gmail.com>
2015-09-20 17:55:53 +1200

    merged

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+import controller.GameController;
+
+// MAIN METHOD
+public class main {
+
+    public static void main(String[] args) throws IOException {
+        new GameController();
+    }
+
+}
