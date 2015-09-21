29fe8597b1b080057e8763e1c1be0e064ec7aa99
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-09-21 13:49:15 +1200

    commented and refactored

--- src/model/factories/TerrainFactory.java
+++ src/model/factories/TerrainFactory.java
@@ -5,6 +5,11 @@ import model.textures.TerrainTexture;
 import model.textures.TerrainTexturePack;
 import model.toolbox.Loader;
 
+/**
+ * Terrain factory to generate new terrains.
+ *
+ * @author Marcel van Workum
+ */
 public class TerrainFactory {
 
     private final Loader loader;
@@ -15,8 +20,16 @@ public class TerrainFactory {
     private final TerrainTexturePack texturePack;
     private final TerrainTexture blendMap;
 
+    /**
+     * Instantiates a new Terrain factory.
+     *
+     * @param loader the loader
+     */
     public TerrainFactory(Loader loader) {
         this.loader = loader;
+
+        // TODO
+
         // Terrain creation
         backgroundTexture = new TerrainTexture(loader.loadTexture("textures/grass"));
         rTexture = new TerrainTexture(loader.loadTexture("textures/mud"));
@@ -25,10 +38,16 @@ public class TerrainFactory {
 
         // Bundle terrains into pack
         texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+
         // Blend map for mixing terrains
         blendMap = new TerrainTexture(loader.loadTexture("terrains/blendMap"));
     }
 
+    /**
+     * Make terrain.
+     *
+     * @return the terrain
+     */
     public Terrain makeTerrain() {
         // Create the new terrain object, using pack blendermap and heightmap
         return new Terrain(0, -1, loader, texturePack, blendMap, "terrains/heightMap");
