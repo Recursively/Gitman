4aefffc207f42eb0278da140e5234b747f0813a1
Divya <1divyap1@gmail.com>
2015-10-02 19:42:10 +1300

    Added bug and laptop character extentions to NPC

---
+++ src/model/entities/movableEntity/Bug.java
@@ -0,0 +1,21 @@
+package model.entities.movableEntity;
+
+import model.models.TexturedModel;
+
+import org.lwjgl.util.vector.Vector3f;
+
+public class Bug extends NonPlayerCharacters {
+
+	public Bug(TexturedModel model, Vector3f position, float rotX, float rotY,
+			float rotZ, float scale, int id) {
+		super(model, position, rotX, rotY, rotZ, scale, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Bug(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale,
+            int textureIndex, int id) {
+		super(model, position, rotX, rotY, rotZ, scale, textureIndex, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
