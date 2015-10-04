79d6fc0988db7f547caa6ae701b0729eed35d433
Divya <1divyap1@gmail.com>
2015-10-04 18:42:20 +1300

    Added NPCCharacter and Bug implementations
    - created classes
    - set out how interactions with the NPC's can be carried out with the
    players
    - added comments

--- src/model/entities/movableEntity/Bug.java
+++ src/model/entities/movableEntity/Bug.java
@@ -5,24 +5,30 @@ import model.models.TexturedModel;
 
 import org.lwjgl.util.vector.Vector3f;
 
+/**
+ * Represents the bug in the game.
+ * 
+ * @author Divya
+ *
+ */
 public class Bug extends NonPlayerCharacters {
 
 	public Bug(TexturedModel model, Vector3f position, float rotX, float rotY,
 			float rotZ, float scale, int id) {
 		super(model, position, rotX, rotY, rotZ, scale, id);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public Bug(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale,
             int textureIndex, int id) {
 		super(model, position, rotX, rotY, rotZ, scale, textureIndex, id);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void interact(GameWorld game) {
-		// TODO Auto-generated method stub
-		
+		// only can 'apply patch' to bug if all commits collected
+		if(game.canApplyPatch()){
+			game.winGame();
+		}
 	}
 
 }
--- src/model/entities/movableEntity/NonPlayerCharacters.java
+++ src/model/entities/movableEntity/NonPlayerCharacters.java
@@ -4,18 +4,24 @@ import model.models.TexturedModel;
 
 import org.lwjgl.util.vector.Vector3f;
 
+/**
+ * Represent a non player character in the game (characters that
+ * are not controlled by players but can be interacted with by
+ * the players in a set way).
+ * 
+ * @author Divya
+ *
+ */
 public abstract class NonPlayerCharacters extends MovableEntity {
 
 	public NonPlayerCharacters(TexturedModel model, Vector3f position,
 			float rotX, float rotY, float rotZ, float scale, int id) {
 		super(model, position, rotX, rotY, rotZ, scale, id);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public NonPlayerCharacters(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale,
             int textureIndex, int id) {
 		super(model, position, rotX, rotY, rotZ, scale, textureIndex, id);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
