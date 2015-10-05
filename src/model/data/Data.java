3da5044cd8faf71c30177e96292f57669726f4fe
Finn Kinnear <finn.kinnear@gmail.com>
2015-10-05 15:28:08 -0700

    Changed Data class to store important fields, and added getters for those fields.

--- src/model/data/Data.java
+++ src/model/data/Data.java
@@ -7,12 +7,36 @@ import model.entities.Camera;
 public class Data {
 	
 	private Vector3f playerVec;
-	private Camera camera;
+	private float pitch;
+	private float roll;
+	private float yaw;
 	private int uid;
 	
 	public Data(Vector3f playerVec, String pitch, String roll, String yaw, String uid){
 		this.playerVec = playerVec;
-		//camera = new Camera();
+		this.pitch = Float.parseFloat(pitch);
+		this.roll = Float.parseFloat(roll);
+		this.yaw = Float.parseFloat(yaw);
 		this.uid = Integer.parseInt(uid);
+	}
+
+	public Vector3f getPlayerVec() {
+		return playerVec;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public int getUid() {
+		return uid;
 	}
 }
