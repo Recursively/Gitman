bf39080de55e0465a17e7f3b44cb18230226e3ee
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-10-13 14:02:07 +1300

    refactored main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,26 +2,37 @@ package main;
 
 import view.TitleScreen;
 
-// MAIN METHOD
+/**
+ * Main method delegate class
+ *
+ * @author Marcel van Workum
+ */
 public class Main {
-    // ARGS ARE BOOLEAN: IS HOST OF THE GAME, STRING: IPADDRESS TO CONNECT TO
 
+    /**
+     * Create the game
+     *
+     * @param args FULLSCREEN (Y/N) IPADDRESS(XXX.XXX.XXX.X.X)
+     */
     public static void main(String[] args) {
 
+        // Checks that the args are valid
         if (args.length < 1 || args.length > 2) {
             System.out.println("Invalid number of arguments");
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             System.out.println("Arguments are: \n   fullscreen (Y/N)\n   ipAddress (xxx.xxx.xxx.x.x)");
         }
 
+        // Parse the full screen args
         boolean fullscreen = false;
-
         if (args[0].equalsIgnoreCase("Y") || args[1].equalsIgnoreCase("True")) {
             fullscreen = true;
         }
 
+        // checks if the player tried to enter an ip address
         boolean isHost = args.length == 1;
 
+        // Creates the correct game client type
         if (!isHost) {
             String ipAddress = args[2];
             new TitleScreen(false, ipAddress, fullscreen);
