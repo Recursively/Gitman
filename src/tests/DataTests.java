27f54db13d371d82b714ba9613abb5d434f3171e
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-10-14 11:38:36 +1300

    more tests

--- src/tests/DataTests.java
+++ src/tests/DataTests.java
@@ -34,22 +34,56 @@ public class DataTests {
     }
 
     @Test
-    public void testCompareGamestateFields() {
+    public void testProgramCompilation() throws Exception {
         initTestGame();
 
         assertTrue("isProgramCompiled comparison",
                 GameWorld.isProgramCompiled() == data.isIsProgramCompiled());
+    }
+
+    @Test
+    public void testIsOutside() throws Exception {
+        initTestGame();
+
         assertTrue("isOutside comparison",
                 GameWorld.isOutside() == data.isIsOutside());
+    }
+
+    @Test
+    public void testProgressComparison() throws Exception {
+        initTestGame();
+
         assertTrue("progress comparison",
                 gameWorld.getProgress() == data.getProgress());
-        assertTrue("score comparison", gameWorld.getScore() == data.getScore());
+    }
+
+    @Test
+    public void testCanApplyPatch() throws Exception {
+        initTestGame();
+
         assertTrue("canApplyPatch comparison",
                 gameWorld.isCanApplyPatch() == data.isCanApplyPatch());
+    }
+
+    @Test
+    public void testCommitIndex() throws Exception {
+        initTestGame();
+
         assertTrue("commitIndex comparison",
                 gameWorld.getCommitIndex() == data.getCommitIndex());
-        assertTrue("timer comparison", gameWorld.getTimer() == Load.loadGame().getTimer());
+    }
 
+    @Test
+    public void testTimerComparison() {
+        initTestGame();
+
+        assertTrue("timer comparison", gameWorld.getTimer() == Load.loadGame().getTimer());
+    }
+
+    @Test
+    public void testScoreComparison() {
+        initTestGame();
+        assertTrue("score comparison", gameWorld.getScore() == data.getScore());
     }
 
     @Test
@@ -61,11 +95,16 @@ public class DataTests {
     }
 
     @Test
-    public void testCompareMovableEntities() {
+    public void testMovableEntitiesCollectionSize() {
         initTestGame();
 
         assertTrue("Collection the same size", data.getMovableEntities().size() ==
                 gameWorld.getMoveableEntities().size());
+    }
+
+    @Test
+    public void testCompareMovableEntities() {
+        initTestGame();
 
         HashSet<Integer> uids = new HashSet<>();
 
