3fd0ec28b174196738b022a7f2e00f2add464029
Divya <1divyap1@gmail.com>
2015-10-13 15:24:16 +1300

    Removed repeated render line of disconnected screen rendering

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -199,7 +199,6 @@ public class GameController {
             }
             
             if (networkDisconnected){
-            	guiRenderer.render(gameWorld.getDisconnectedScreen());
             	while(true){
             		guiRenderer.render(gameWorld.getDisconnectedScreen());
             		DisplayManager.updateDisplay();
